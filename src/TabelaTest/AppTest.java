da8806412e1570947a02de89a8fbc100009d37ca
AntonioNeto98 <141379093+AntonioNeto98@users.noreply.github.com>
2024-11-14 12:30:07 -0300

    first commit

---
+++ src/TabelaTest/AppTest.java
@@ -0,0 +1,14 @@
+package TabelaTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        String sexo = "Feminino";
+        Assert.assertEquals("Feminino", sexo);
+    }
+
+}
